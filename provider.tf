62d1111609bc31ece7fc7f20a5efe98dccd4e1a8
J.L. Álvarez <alvarez@uhu.es>
2020-03-24 14:27:53 +0000

    Commit Inicial

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "ASIASA7D5YWLW5NMLEOE"
-  secret_key = "pb/Wi8nntLlPwascGUvkzF7+zFAvfBURZT8I+Lfh"
-  token      = "FwoGZXIvYXdzEHQaDNZ0qpzWK7wlbb5VUiK9AT/Il2syoF6jWG7Kqmtsad8wuHGDYyFNqRNPr/SLjWQV8M1NQc/cJ3PcLdGWHIhntTfogSR6VLsjWpeJA2RPSW1i664LqDqSvkRLfJuQ0xJ8HRrWaXfWrmSA4b2alh/8HSUKaItSGrPE2OfyJ5d3QkEt9yVzt11dJGzR6AlQ1FqmRsar1rxXSyxRfp5k49EeK9Zu9BRqqR1iesEmKADF9pCwUFZyhSXlgk+Ii2VKDxE+OUEuI8KFAyinohievSi90efzBTIt08Td7eiaVblhLtvE1U32vJbsJV0r+qdr3jPTsTZytpMM97Sr66b4pUnrasrk"
+  access_key = "ACCESSKEY"
+  secret_key = "SECRETKEY"
+  token      = "SESSIONTOKEN"
   
 }
 
